0fd5c5ce5d7e6507faf4f4f5c5853a494d954cde
dahabjones <dahab_jones@cable.comcast.com>
2021-07-01 17:46:54 -0400

    Studio 5

--- src/test/java/BalancedBracketsTests.java
+++ src/test/java/BalancedBracketsTests.java
@@ -13,4 +13,45 @@ public class BalancedBracketsTests {
     public void testBasicBrackets() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    @Test
+    public void testBracketsWithStrings() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void testBracketsInMiddleString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testBracketsInFrontString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void testBasicParOnly() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testBasicLC() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testBasicBackBracketString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void testBasicOneBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testBasicBackwardBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 }
+
